4b880a8a8208f748bca4514e1954a8283cd044b0
Your Name <you@example.com>
2020-10-04 15:36:50 +0530

    Updates

---
+++ precache-manifest.abbae01b1ce0d4df750d847980999af3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9125ac5c0fa12088c878f9a692cb9797",
+    "url": "/github-repo-bookmark-manager/index.html"
+  },
+  {
+    "revision": "25fe689fd8e2171ce4f7",
+    "url": "/github-repo-bookmark-manager/static/css/main.a2866698.chunk.css"
+  },
+  {
+    "revision": "9a9e7d6272cab1b55993",
+    "url": "/github-repo-bookmark-manager/static/js/2.e657d8b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-repo-bookmark-manager/static/js/2.e657d8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fe689fd8e2171ce4f7",
+    "url": "/github-repo-bookmark-manager/static/js/main.b726f8ac.chunk.js"
+  },
+  {
+    "revision": "29fe3aeffcd140c9f9fe",
+    "url": "/github-repo-bookmark-manager/static/js/runtime-main.74eb5478.js"
+  },
+  {
+    "revision": "5b1f481cf48d9559db329a8b0458730c",
+    "url": "/github-repo-bookmark-manager/static/media/github.5b1f481c.jpg"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "/github-repo-bookmark-manager/static/media/logo.e45ebd52.png"
+  },
+  {
+    "revision": "427b8bc29df2dae700937053281fcaa7",
+    "url": "/github-repo-bookmark-manager/static/media/navlogo.427b8bc2.png"
+  },
+  {
+    "revision": "5e39cdaae9d6c9ada5c2de4b2cf416ae",
+    "url": "/github-repo-bookmark-manager/static/media/white.5e39cdaa.jpg"
+  }
+]);
